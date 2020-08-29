98f82c6ac37fdcae891f4b0ae56ab1a7a946ec1f
Jummit <jummit@web.de>
2020-08-29 16:14:42 +0200

    add masked blending viewport

--- material_layers/masked_texture_blending_viewport/masked_texture_blending_viewport.gd
+++ material_layers/masked_texture_blending_viewport/masked_texture_blending_viewport.gd
@@ -1,2 +1,6 @@
 extends "res://addons/blending/texture_blending_viewport.gd"
 
+func setup_sprite(sprite : Sprite, options : Dictionary) -> void:
+	sprite.material = ShaderMaterial.new()
+	sprite.material.shader = preload("res://material_layers/masked_texture_blending_viewport/masked_overlay_shader.shader")
+	sprite.material.set_shader_param("mask", options.mask)
