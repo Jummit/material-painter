89e554e65ccaf5e0e42e276a31fbcb66ce4125d2
Jummit <jummit@web.de>
2020-08-29 11:02:17 +0200

    remove default textures

--- texture_manager.gd
+++ texture_manager.gd
@@ -1,10 +1,4 @@
 extends Node
 
-const TextureMap = preload("res://texture_map.gd")
-
 # warning-ignore:unused_class_variable
-var textures := [
-	TextureMap.new("Test Texture"),
-	TextureMap.new("GrayScale"),
-	TextureMap.new("Something"),
-]
+var textures := []
