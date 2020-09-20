2c88e96339ad6ed074a565faa476993ec2d0e73f
Jummit <jummit@web.de>
2020-09-20 20:31:48 +0200

    more shaders and setup

--- 3d_viewport/painting/paint_viewport/paint_viewport.gd
+++ 3d_viewport/painting/paint_viewport/paint_viewport.gd
@@ -1,1 +1,14 @@
 extends Viewport
+
+onready var paint_rect : ColorRect = $PaintRect
+onready var paint_material : ShaderMaterial = $PaintRect.material
+
+func load_utility_textures(textures : Dictionary) -> void:
+	paint_material.set_shader_param("tex2view_tex", textures.texture_to_view)
+	paint_material.set_shader_param("seams", textures.seams)
+
+
+func paint(position : Vector2, previous_position : Vector2) -> void:
+	paint_material.set_shader_param("brush_pos", position)
+	paint_material.set_shader_param("brush_ppos", previous_position)
+	render_target_update_mode = Viewport.UPDATE_ONCE
--- 3d_viewport/painting/utility_texture_viewports/utility_texture_viewports.gd
+++ 3d_viewport/painting/utility_texture_viewports/utility_texture_viewports.gd
@@ -1,1 +1,22 @@
 extends Node
+
+onready var texture_to_view_mesh_instance : MeshInstance = $TextureToViewViewport/MeshInstance
+onready var view_to_texture_mesh_instance : MeshInstance = $ViewToTextureViewport/MeshInstance
+onready var view_to_texture_viewport : Viewport = $ViewToTextureViewport
+onready var texture_to_view_viewport : Viewport = $TextureToViewViewport
+onready var seams_viewport : Viewport = $SeamsViewport
+
+func generate_textures_from_mesh(mesh : Mesh) -> Dictionary:
+	texture_to_view_mesh_instance.mesh = mesh
+	view_to_texture_mesh_instance.mesh = mesh
+	view_to_texture_viewport.render_target_update_mode = Viewport.UPDATE_ONCE
+	yield(VisualServer, "frame_post_draw")
+	texture_to_view_viewport.render_target_update_mode = Viewport.UPDATE_ONCE
+	yield(VisualServer, "frame_post_draw")
+	seams_viewport.render_target_update_mode = Viewport.UPDATE_ONCE
+	yield(VisualServer, "frame_post_draw")
+	return {
+		view_to_texture = view_to_texture_viewport.get_texture(),
+		texture_to_view = texture_to_view_viewport.get_texture(),
+		seams = seams_viewport.get_texture(),
+	}
