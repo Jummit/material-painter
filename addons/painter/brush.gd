046e823d181d78a33afcd63a48ad976ea3c28111
Jummit <jummit@web.de>
2020-09-25 09:08:26 +0200

    add brush support to painter

---
+++ addons/painter/brush.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+export var size := 1.0
+export var strength := 1.0
+export var texture : Texture
+export var color := Color.white
+export var pattern_scale := 1.0
+export var texture_angle := 0.0
+export var stamp_mode := false
+export var texture_mask : Texture
--- painting/brush.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-
-export var opacity := 1.0
-export var size := 1.0
