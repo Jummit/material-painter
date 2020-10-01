d5acfb1d8149dfb8d7e71e3186b653b3b6417eef
Jummit <jummit@web.de>
2020-10-01 20:46:50 +0200

    limit camera controls to viewport

--- addons/rotating_camera/rotating_camera.gd
+++ addons/rotating_camera/rotating_camera.gd
@@ -11,10 +11,10 @@ onready var horizontal_camera_socket : Spatial = get_parent()
 onready var vertical_camera_socket : Spatial = get_parent().get_parent()
 
 func _input(event : InputEvent) -> void:
-	if event is InputEventMouseButton and (event.button_index == BUTTON_WHEEL_UP or event.button_index == BUTTON_WHEEL_DOWN):
+	if event is InputEventMouseButton and (event.button_index == BUTTON_WHEEL_UP or event.button_index == BUTTON_WHEEL_DOWN) and get_viewport().get_visible_rect().has_point(event.position):
 		translation.z += (1.0 if event.button_index == BUTTON_WHEEL_DOWN else -1.0) * zoom_sensitity
 		translation.z = clamp(translation.z, .4, 20.0)
-	if event is InputEventMouseMotion and event.button_mask == BUTTON_MASK_MIDDLE:
+	if event is InputEventMouseMotion and event.button_mask == BUTTON_MASK_MIDDLE and get_viewport().get_visible_rect().has_point(event.position):
 		if event.shift:
 			vertical_camera_socket.translate_object_local(Vector3.LEFT * event.relative.x * sensitity)
 			vertical_camera_socket.global_transform.origin += global_transform.basis.y * event.relative.y * sensitity
