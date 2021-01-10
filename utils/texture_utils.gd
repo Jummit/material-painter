218c50948c8b528403a986bd9c219b2057b32cbd
Jummit <jummit@web.de>
2021-01-10 23:54:44 +0100

    make saved viewport textures have alpha

--- utils/texture_utils.gd
+++ utils/texture_utils.gd
@@ -5,5 +5,7 @@ independent of the `Viewport`
 
 static func viewport_to_image(viewport_texture : ViewportTexture) -> ImageTexture:
 	var image_texture := ImageTexture.new()
-	image_texture.create_from_image(viewport_texture.get_data())
+	var image := viewport_texture.get_data()
+	image.convert(Image.FORMAT_RGBA8)
+	image_texture.create_from_image(image)
 	return image_texture
