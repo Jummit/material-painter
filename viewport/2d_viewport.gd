70593706c5bd8519b1d4bd0e0e5e00d3597d780e
Jummit <jummit@web.de>
2021-01-12 09:52:27 +0100

    add all surfaces to 2D UV mesh

--- viewport/2d_viewport.gd
+++ viewport/2d_viewport.gd
@@ -6,10 +6,11 @@ func _prepare_mesh(mesh : Mesh) -> Mesh:
 
 static func uv_to_vertex_positions(mesh : Mesh) -> Mesh:
 	var data_tool := MeshDataTool.new()
-	data_tool.create_from_surface(mesh, 0)
-	for vertex in data_tool.get_vertex_count():
-		var uv := data_tool.get_vertex_uv(vertex)
-		data_tool.set_vertex(vertex, Vector3(uv.x, 1.0 - uv.y, 0))
 	var new_mesh := Mesh.new()
-	data_tool.commit_to_surface(new_mesh)
+	for surface in mesh.get_surface_count():
+		data_tool.create_from_surface(mesh, 0)
+		for vertex in data_tool.get_vertex_count():
+			var uv := data_tool.get_vertex_uv(vertex)
+			data_tool.set_vertex(vertex, Vector3(uv.x, 1.0 - uv.y, 0))
+		data_tool.commit_to_surface(new_mesh)
 	return new_mesh
