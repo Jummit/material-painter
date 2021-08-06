6badb37ff4b1b63a9b0f3346fe2e177826b60330
Jummit <jummit@web.de>
2021-08-06 22:46:18 +0200

    Fix editor scripts

--- utils/editor/generate_constructor.gd
+++ utils/editor/generate_constructor.gd
@@ -17,7 +17,6 @@ func _run() -> void:
 	
 	var declarations := OS.clipboard.replace("	", "").split("\n")
 	for declaration in declarations:
-		declaration = declaration as String
 		var words : PoolStringArray = declaration.split(" ")
 		args += "_" + declaration.trim_prefix("var ") + ", "
 		assigns += "	%s = _%s\n" % [words[1], words[1]]
--- utils/editor/name_buttons.gd
+++ utils/editor/name_buttons.gd
@@ -7,5 +7,5 @@ A utility to name `Button` nodes after their text and class type
 
 func _run():
 	for button in get_editor_interface().get_selection().get_selected_nodes():
-		button = button as Button
-		button.name = button.text.replace(" ", "") + button.get_class()
+		if button is Button:
+			button.name = button.text.replace(" ", "") + button.get_class()
