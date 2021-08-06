c9d90765b2d4a2dcb80af06e86f52dfa02d11576
Jummit <jummit@web.de>
2021-08-06 22:40:09 +0200

    Organize methods and improve OBJ import progress

--- main/main.gd
+++ main/main.gd
@@ -14,10 +14,10 @@ signal current_layer_material_changed(to, id)
 signal mesh_changed(to)
 signal context_changed(to)
 
-var current_file : ProjectFile setget set_current_file
-var current_layer_material : MaterialLayerStack setget set_current_layer_material
+var current_file : ProjectFile setget _set_current_file
+var current_layer_material : MaterialLayerStack setget _set_current_layer_material
 var undo_redo := UndoRedo.new()
-var context : MaterialGenerationContext
+var _generation_context : MaterialGenerationContext
 
 # To avoid https://github.com/godotengine/godot/issues/36895, this is passed to
 # `add_do_action` instead of null.
@@ -83,14 +83,16 @@ onready var load_file_error_dialog : AcceptDialog = $LoadFileErrorDialog
 onready var mesh_maps_generator : MeshMapsGenerator = $MeshMapsGenerator
 
 func _ready() -> void:
-	context = MaterialGenerationContext.new(layer_blend_viewport_manager,
-			normal_map_generation_viewport, triplanar_texture_generator)
+	_generation_context = MaterialGenerationContext.new(
+			layer_blend_viewport_manager, normal_map_generation_viewport,
+			triplanar_texture_generator)
 	
 # warning-ignore:unsafe_property_access
 	ProgressDialogManager.theme = theme
 	
 	undo_redo.connect("version_changed", self, "_on_UndoRedo_version_changed")
-	file_menu_button.get_popup().connect("id_pressed", self, "_on_FileMenu_id_pressed")
+	file_menu_button.get_popup().connect("id_pressed", self,
+			"_on_FileMenu_id_pressed")
 	about_menu_button.get_popup().connect("id_pressed", self,
 			"_on_AboutMenuButton_id_pressed")
 	
@@ -122,8 +124,8 @@ func _ready() -> void:
 		]
 	})
 	keymap_screen.load_keymap("user://keymap.json")
-	initialise_layouts()
-	start_empty_project()
+	_initialise_layouts()
+	_start_empty_project()
 
 
 func _input(event : InputEvent) -> void:
@@ -140,54 +142,37 @@ func _input(event : InputEvent) -> void:
 			print("Redo: " + undo_redo.get_current_action_name())
 
 
-func _on_FileDialog_file_selected(path : String) -> void:
-	match file_dialog.mode:
-		FileDialog.MODE_SAVE_FILE:
-			current_file.path = path
-		FileDialog.MODE_OPEN_FILE:
-			match path.get_extension():
-				"mproject":
-					var file := File.new()
-					if file.open(path, File.READ) != OK:
-						return
-					var result = JSON.parse(file.get_as_text())
-					if result.error:
-						load_file_error_dialog.dialog_text = "Can't parse json: Content is corrupted.\n At line %s:\n%s" % [result.error_line, result.error_string]
-						load_file_error_dialog.popup_centered()
-						return
-					var project := ProjectFile.new(result.result)
-					file.close()
-					project.path = path
-					set_current_file(project)
-				"obj":
-					load_mesh(path)
+# Material manipulation
+
+func _set_current_layer_material(to) -> void:
+	current_layer_material = to
+	current_layer_material.context = _generation_context
+	emit_signal("current_layer_material_changed", to,
+			current_file.layer_materials.find(current_layer_material))
 
 
-func _on_AddButton_pressed() -> void:
-	do_add_layer(MaterialLayer.new())
+func _set_mask(layer, mask : TextureLayerStack) -> void:
+	if mask == NO_MASK:
+		layer.mask = null
+	else:
+		layer.mask = mask
+		mask.parent = layer
+		mask.mark_dirty()
+	layer.mark_dirty()
+	current_layer_material.update()
 
 
-func _on_AddPaintLayerButton_pressed() -> void:
-	var new_layer := MaterialLayer.new()
-	new_layer.hide_first_layer = true
-	new_layer.main.add_layer(PaintTextureLayer.new())
-	do_add_layer(new_layer)
+func _do_change_mask_action(action_name : String, layer,
+		mask : TextureLayerStack) -> void:
+	undo_redo.create_action(action_name)
+	undo_redo.add_do_method(self, "_set_mask", layer, mask)
+	undo_redo.add_do_method(layer_tree, "reload")
+	undo_redo.add_undo_method(self, "_set_mask", layer, layer.mask)
+	undo_redo.add_undo_method(layer_tree, "reload")
+	undo_redo.commit_action()
 
 
-func _on_AddFillLayerButton_pressed() -> void:
-	var new_layer := MaterialLayer.new()
-	new_layer.hide_first_layer = true
-	new_layer.main.add_layer(FillTextureLayer.new())
-	do_add_layer(new_layer)
-
-
-func _on_AddFolderLayerButton2_pressed() -> void:
-	var new_layer := MaterialLayer.new()
-	new_layer.is_folder = true
-	do_add_layer(new_layer)
-
-
-func do_add_layer(layer : MaterialLayer) -> void:
+func _do_add_layer(layer : MaterialLayer) -> void:
 	var onto
 	var selected_mat : MaterialLayer = layer_tree.get_selected_layer() as MaterialLayer
 	if layer.main.layers.size():
@@ -206,6 +191,185 @@ func do_add_layer(layer : MaterialLayer) -> void:
 			layer)
 	undo_redo.add_undo_method(layer_tree, "reload")
 	undo_redo.commit_action()
+
+
+# I/O
+
+func _start_empty_project() -> void:
+	_set_current_file(ProjectFile.new({
+		model_path = "res://misc/cube.obj"
+	}))
+
+
+func _set_current_file(to : ProjectFile) -> void:
+	current_file = to
+	_generation_context.result_size = current_file.result_size
+	for layer_material in current_file.layer_materials:
+		layer_material.context = _generation_context
+		var result = layer_material.update()
+		while result is GDScriptFunctionState:
+			result = yield(result, "completed")
+	var result = _load_mesh(current_file.model_path)
+	while result is GDScriptFunctionState:
+		result = yield(result, "completed")
+	_set_current_layer_material(current_file.layer_materials.front())
+	emit_signal("context_changed", _generation_context)
+	emit_signal("current_file_changed", current_file)
+
+
+func _load_project_file(path : String) -> void:
+	var file := File.new()
+	if file.open(path, File.READ) != OK:
+		return
+	var result = JSON.parse(file.get_as_text())
+	if result.error:
+		load_file_error_dialog.dialog_text =\
+				"Can't parse json: Content is corrupted.\n At line %s:\n%s" % [
+				result.error_line, result.error_string]
+		load_file_error_dialog.popup_centered()
+		return
+	var project := ProjectFile.new(result.result)
+	file.close()
+	project.path = path
+	_set_current_file(project)
+
+
+const POLLS_PER_FRAME := 30000
+func _load_mesh(path : String) -> void:
+	var interactive_loader := ObjParser.parse_obj_interactive(path)
+# warning-ignore:integer_division
+	var stage_count := interactive_loader.get_stage_count()
+# warning-ignore:unsafe_method_access
+	var progress_dialog = ProgressDialogManager.create_task("Load OBJ Model",
+			stage_count)
+	var new_mesh : ArrayMesh
+	while not new_mesh:
+		for _i in POLLS_PER_FRAME:
+			new_mesh = interactive_loader.poll()
+			if new_mesh:
+				break
+		var stage := interactive_loader.get_stage()
+		progress_dialog.set_action("Stage %s / %s" % [stage, stage_count], stage)
+		yield(get_tree(), "idle_frame")
+	progress_dialog.complete_task()
+	_generation_context.mesh = new_mesh
+	current_file.model_path = path
+	for surface in _generation_context.mesh.get_surface_count():
+		if current_file.layer_materials.size() <= surface:
+			current_file.layer_materials.append(MaterialLayerStack.new())
+	current_file.layer_materials.front().update()
+	emit_signal("mesh_changed", _generation_context.mesh)
+	emit_signal("layer_materials_changed", current_file.layer_materials)
+	_set_current_layer_material(current_file.layer_materials.front())
+
+
+# Save the current project, prompt the user for a file path if the file is saved
+# for the first time.
+func _request_save_file() -> void:
+	if not current_file.path:
+		_open_save_project_dialog()
+		yield(file_dialog, "file_selected")
+	var file := File.new()
+	file.open(current_file.path, File.WRITE)
+	file.store_string(to_json(current_file.serialize()))
+	file.close()
+
+
+func _open_save_project_dialog() -> void:
+	file_dialog.mode = FileDialog.MODE_SAVE_FILE
+	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
+	file_dialog.filters = ["*.mproject;Material Painter File"]
+	file_dialog.current_dir = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
+	file_dialog.current_file = ""
+	file_dialog.set_meta("to_save", current_file)
+	file_dialog.popup_centered()
+
+
+func _export_materials() -> void:
+# warning-ignore:unsafe_method_access
+	var progress_dialog = ProgressDialogManager.create_task("Export Textures",
+			current_layer_material.results.size())
+	yield(get_tree(), "idle_frame")
+	for surface in current_file.layer_materials.size():
+		var material_name := _generation_context.mesh.surface_get_material(
+				surface).resource_name
+		var export_folder := current_file.path.get_base_dir().\
+				plus_file("export").plus_file(material_name)
+		var dir := Directory.new()
+		dir.make_dir_recursive(export_folder)
+		var results : Dictionary = current_file.layer_materials[surface].results
+		for type in results:
+			progress_dialog.set_action("%s of %s" % [type, material_name])
+			var result_data : Image = results[type].get_data()
+			result_data.save_png(export_folder.plus_file(type) + ".png")
+			yield(get_tree(), "idle_frame")
+	progress_dialog.complete_task()
+
+
+# Misc
+
+func _do_quit():
+	keymap_screen.save_keymap("user://keymap.json")
+	get_tree().quit()
+
+
+func _initialise_layouts() -> void:
+	# Wait for all windows to be ready.
+	yield(get_tree(), "idle_frame")
+	var dir := Directory.new()
+	dir.make_dir_recursive("user://layouts")
+	var default := LAYOUTS_FOLDER.plus_file("default.json")
+	if not dir.file_exists(default):
+		LayoutUtils.save_layout(root.get_child(0), default)
+	else:
+		LayoutUtils.load_layout(root, default)
+	view_menu_button.update_layout_options()
+
+
+# Signal callbacks
+
+func _on_TextureMapButtons_maps_changed() -> void:
+	current_layer_material.update()
+
+
+func _on_SurfaceList_surface_selected(surface : int) -> void:
+	_set_current_layer_material(current_file.layer_materials[surface])
+
+
+func _on_FileDialog_file_selected(path : String) -> void:
+	match file_dialog.mode:
+		FileDialog.MODE_SAVE_FILE:
+			current_file.path = path
+		FileDialog.MODE_OPEN_FILE:
+			match path.get_extension():
+				"mproject":
+					_load_project_file(path)
+				"obj":
+					_load_mesh(path)
+
+
+func _on_AddButton_pressed() -> void:
+	_do_add_layer(MaterialLayer.new())
+
+
+func _on_AddPaintLayerButton_pressed() -> void:
+	var new_layer := MaterialLayer.new()
+	new_layer.hide_first_layer = true
+	new_layer.main.add_layer(PaintTextureLayer.new())
+	_do_add_layer(new_layer)
+
+
+func _on_AddFillLayerButton_pressed() -> void:
+	var new_layer := MaterialLayer.new()
+	new_layer.hide_first_layer = true
+	new_layer.main.add_layer(FillTextureLayer.new())
+	_do_add_layer(new_layer)
+
+
+func _on_AddFolderLayerButton2_pressed() -> void:
+	var new_layer := MaterialLayer.new()
+	new_layer.is_folder = true
+	_do_add_layer(new_layer)
 
 
 func _on_DeleteButton_pressed() -> void:
@@ -259,16 +423,16 @@ func _on_MaterialLayerPopupMenu_layer_saved() -> void:
 
 
 func _on_MaterialLayerPopupMenu_mask_added(mask : TextureLayerStack) -> void:
-	do_change_mask_action("Add Mask", layer_tree.get_selected_layer(), mask)
+	_do_change_mask_action("Add Mask", layer_tree.get_selected_layer(), mask)
 
 
 func _on_MaterialLayerPopupMenu_mask_removed() -> void:
-	do_change_mask_action("Remove Mask", layer_tree.get_selected_layer(),
+	_do_change_mask_action("Remove Mask", layer_tree.get_selected_layer(),
 			NO_MASK)
 
 
 func _on_MaterialLayerPopupMenu_mask_pasted(mask : TextureLayerStack) -> void:
-	do_change_mask_action("Paste Mask", layer_tree.get_selected_layer(), mask)
+	_do_change_mask_action("Paste Mask", layer_tree.get_selected_layer(), mask)
 
 
 func _on_MaterialLayerPopupMenu_duplicated() -> void:
@@ -309,17 +473,19 @@ func _on_EditMenuButton_bake_mesh_maps_pressed() -> void:
 # warning-ignore:unsafe_method_access
 	var progress_dialog = ProgressDialogManager.create_task("Bake Mesh Maps",
 			mesh_maps_generator.BAKE_FUNCTIONS.size() *\
-			context.mesh.get_surface_count())
+			_generation_context.mesh.get_surface_count())
 	
-	for surface in context.mesh.get_surface_count():
-		var mat_name := context.mesh.surface_get_material(surface).resource_name
+	for surface in _generation_context.mesh.get_surface_count():
+		var mat_name := _generation_context.mesh.surface_get_material(surface)\
+				.resource_name
 		for map in mesh_maps_generator.BAKE_FUNCTIONS:
 			var file := texture_dir.plus_file(mat_name + map) + ".png"
 			progress_dialog.set_action("%s: %s" % [mat_name, file])
 			
 			var result : ImageTexture = yield(
-					mesh_maps_generator.generate_mesh_map(map, context.mesh,
-					Vector2(1024, 1024), surface), "completed")
+					mesh_maps_generator.generate_mesh_map(map,
+					_generation_context.mesh, Vector2(1024, 1024), surface),
+					"completed")
 			
 			result.get_data().save_png(file)
 			asset_store.load_asset(file, TextureAsset)
@@ -329,7 +495,7 @@ func _on_EditMenuButton_bake_mesh_maps_pressed() -> void:
 
 
 func _on_EditMenuButton_size_selected(size) -> void:
-	context.result_size = size
+	_generation_context.result_size = size
 	current_layer_material.update()
 
 
@@ -355,30 +521,17 @@ func _on_ViewMenuButton_save_layout_selected() -> void:
 
 
 func _on_QuitConfirmationDialog_custom_action(_action : String) -> void:
-	do_quit()
+	_do_quit()
 
 
 func _on_QuitConfirmationDialog_confirmed() -> void:
-	var result = request_save_file()
+	var result = _request_save_file()
 	while result is GDScriptFunctionState:
 		result = yield(result, "completed")
-	do_quit()
+	_do_quit()
 
 
-func do_quit():
-	keymap_screen.save_keymap("user://keymap.json")
-	get_tree().quit()
-
-
-func request_save_file() -> void:
-	if not current_file.path:
-		open_save_project_dialog()
-		yield(file_dialog, "file_selected")
-	var file := File.new()
-	file.open(current_file.path, File.WRITE)
-	file.store_string(to_json(current_file.serialize()))
-	file.close()
-
+# Callbacks of procedural connected signals
 
 func _on_AboutMenuButton_id_pressed(id : int) -> void:
 	match id:
@@ -397,7 +550,7 @@ func _on_AboutMenuButton_id_pressed(id : int) -> void:
 func _on_FileMenu_id_pressed(id : int) -> void:
 	match id:
 		FILE_MENU_ITEMS.NEW:
-			start_empty_project()
+			_start_empty_project()
 		FILE_MENU_ITEMS.OPEN:
 			file_dialog.mode = FileDialog.MODE_OPEN_FILE
 			file_dialog.access = FileDialog.ACCESS_FILESYSTEM
@@ -406,9 +559,9 @@ func _on_FileMenu_id_pressed(id : int) -> void:
 			file_dialog.current_file = ""
 			file_dialog.popup_centered()
 		FILE_MENU_ITEMS.SAVE:
-			request_save_file()
+			_request_save_file()
 		FILE_MENU_ITEMS.SAVE_AS:
-			open_save_project_dialog()
+			_open_save_project_dialog()
 			yield(file_dialog, "file_selected")
 			var file := File.new()
 			file.open(current_file.path, File.WRITE)
@@ -416,7 +569,7 @@ func _on_FileMenu_id_pressed(id : int) -> void:
 			file.close()
 		FILE_MENU_ITEMS.EXPORT:
 			if current_file.path:
-				export_materials()
+				_export_materials()
 			else:
 				export_error_dialog.popup_centered()
 		FILE_MENU_ITEMS.LOAD_MESH:
@@ -435,131 +588,3 @@ func _on_FileMenu_id_pressed(id : int) -> void:
 func _on_UndoRedo_version_changed() -> void:
 	if undo_redo.get_current_action_name():
 		print(undo_redo.get_current_action_name())
-
-
-func export_materials() -> void:
-# warning-ignore:unsafe_method_access
-	var progress_dialog = ProgressDialogManager.create_task("Export Textures",
-			current_layer_material.results.size())
-	yield(get_tree(), "idle_frame")
-	for surface in current_file.layer_materials.size():
-		var material_name := context.mesh.surface_get_material(
-				surface).resource_name
-		var export_folder := current_file.path.get_base_dir().plus_file("export")\
-				.plus_file(material_name)
-		var dir := Directory.new()
-		dir.make_dir_recursive(export_folder)
-		var results : Dictionary = current_file.layer_materials[surface].results
-		for type in results:
-			progress_dialog.set_action("%s of %s" % [type, material_name])
-			var result_data : Image = results[type].get_data()
-			result_data.save_png(export_folder.plus_file(type) + ".png")
-			yield(get_tree(), "idle_frame")
-	progress_dialog.complete_task()
-
-
-func set_mask(layer, mask : TextureLayerStack) -> void:
-	if mask == NO_MASK:
-		layer.mask = null
-	else:
-		layer.mask = mask
-		mask.parent = layer
-		mask.mark_dirty()
-	layer.mark_dirty()
-	current_layer_material.update()
-
-
-func load_mesh(path : String) -> void:
-	var interactive_loader := ObjParser.parse_obj_interactive(path)
-	var stage_count := int(interactive_loader.get_stage_count() / 20000.0)
-# warning-ignore:unsafe_method_access
-	var progress_dialog = ProgressDialogManager.create_task("Load OBJ Model",
-			stage_count)
-	while true:
-		progress_dialog.set_action("Stage %s / %s" % [
-				interactive_loader.get_stage(), stage_count])
-		yield(get_tree(), "idle_frame")
-		for i in 20000:
-			var new_mesh = interactive_loader.poll()
-			if not new_mesh:
-				continue
-			progress_dialog.complete_task()
-			context.mesh = new_mesh
-			current_file.model_path = path
-			for surface in context.mesh.get_surface_count():
-				if current_file.layer_materials.size() <= surface:
-					current_file.layer_materials.append(MaterialLayerStack.new())
-			current_file.layer_materials.front().update()
-			emit_signal("mesh_changed", context.mesh)
-			emit_signal("layer_materials_changed", current_file.layer_materials)
-			set_current_layer_material(current_file.layer_materials.front())
-			return
-
-
-func start_empty_project() -> void:
-	set_current_file(ProjectFile.new({
-		model_path = "res://misc/cube.obj"
-	}))
-
-
-func open_save_project_dialog() -> void:
-	file_dialog.mode = FileDialog.MODE_SAVE_FILE
-	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
-	file_dialog.filters = ["*.mproject;Material Painter File"]
-	file_dialog.current_dir = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
-	file_dialog.current_file = ""
-	file_dialog.set_meta("to_save", current_file)
-	file_dialog.popup_centered()
-
-
-func do_change_mask_action(action_name : String, layer, mask : TextureLayerStack) -> void:
-	undo_redo.create_action(action_name)
-	undo_redo.add_do_method(self, "set_mask", layer, mask)
-	undo_redo.add_do_method(layer_tree, "reload")
-	undo_redo.add_undo_method(self, "set_mask", layer, layer.mask)
-	undo_redo.add_undo_method(layer_tree, "reload")
-	undo_redo.commit_action()
-
-
-func initialise_layouts() -> void:
-	# Wait for all windows to be ready.
-	yield(get_tree(), "idle_frame")
-	var dir := Directory.new()
-	dir.make_dir_recursive("user://layouts")
-	var default := LAYOUTS_FOLDER.plus_file("default.json")
-	if not dir.file_exists(default):
-		LayoutUtils.save_layout(root.get_child(0), default)
-	else:
-		LayoutUtils.load_layout(root, default)
-	view_menu_button.update_layout_options()
-
-
-func set_current_file(save_file : ProjectFile) -> void:
-	current_file = save_file
-	context.result_size = current_file.result_size
-	for layer_material in current_file.layer_materials:
-		layer_material.context = context
-		var result = layer_material.update()
-		while result is GDScriptFunctionState:
-			result = yield(result, "completed")
-	var result = load_mesh(current_file.model_path)
-	while result is GDScriptFunctionState:
-		result = yield(result, "completed")
-	set_current_layer_material(current_file.layer_materials.front())
-	emit_signal("context_changed", context)
-	emit_signal("current_file_changed", current_file)
-
-
-func set_current_layer_material(to) -> void:
-	current_layer_material = to
-	current_layer_material.context = context
-	emit_signal("current_layer_material_changed", to,
-			current_file.layer_materials.find(current_layer_material))
-
-
-func _on_TextureMapButtons_maps_changed() -> void:
-	current_layer_material.update()
-
-
-func _on_SurfaceList_surface_selected(surface : int) -> void:
-	set_current_layer_material(current_file.layer_materials[surface])
